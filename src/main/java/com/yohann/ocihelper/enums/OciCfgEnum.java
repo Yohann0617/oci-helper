1fd9488071d3d843472817dea09de22160bec82b
Yohann0617 <852221040@qq.com>
2024-11-25 00:23:08 +0800

    update

--- src/main/java/com/yohann/ocihelper/enums/OciCfgEnum.java
+++ src/main/java/com/yohann/ocihelper/enums/OciCfgEnum.java
@@ -16,11 +16,11 @@ public enum OciCfgEnum {
     /**
      * OCI配置
      */
-    OCI_CFG_USER_ID("userId", "用户id"),
-    OCI_CFG_TENANT_ID("tenantId", "租户id"),
+    OCI_CFG_USER_ID("user", "用户id"),
+    OCI_CFG_TENANT_ID("tenancy", "租户id"),
     OCI_CFG_REGION("region", "区域"),
     OCI_CFG_FINGERPRINT("fingerprint", "指纹"),
-    OCI_CFG_KEY_FILE("key_path", "密钥文件全路径"),
+    OCI_CFG_KEY_FILE("key_file", "密钥文件全路径"),
     ;
 
     private String type;
