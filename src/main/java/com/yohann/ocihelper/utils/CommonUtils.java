b081c3d008bbd07e9c3d15cb8daa9ad9e9547dc0
fanyuhui <852221040@qq.com>
2024-11-27 19:27:50 +0800

    update: 修改首次登录脚本

--- src/main/java/com/yohann/ocihelper/utils/CommonUtils.java
+++ src/main/java/com/yohann/ocihelper/utils/CommonUtils.java
@@ -300,6 +300,18 @@ public class CommonUtils {
                 "      sed -i 's/^#\\?PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config\n" +
                 "      sed -i 's/^#\\?PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config\n" +
                 "      \n" +
+                "      # Ensure PrintMotd is set to yes\n" +
+                "      if grep -q \"^#\\?PrintMotd\" /etc/ssh/sshd_config; then\n" +
+                "        sed -i 's/^#\\?PrintMotd.*/PrintMotd yes/' /etc/ssh/sshd_config\n" +
+                "      else\n" +
+                "        echo \"PrintMotd yes\" >> /etc/ssh/sshd_config\n" +
+                "      fi\n" +
+                "      # Ensure PrintLastLog is set to yes\n" +
+                "      if grep -q \"^#\\?PrintLastLog\" /etc/ssh/sshd_config; then\n" +
+                "        sed -i 's/^#\\?PrintLastLog.*/PrintLastLog yes/' /etc/ssh/sshd_config\n" +
+                "      else\n" +
+                "        echo \"PrintLastLog yes\" >> /etc/ssh/sshd_config\n" +
+                "      fi\n\n"+
                 "      # Restart SSH service\n" +
                 "      if command -v systemctl >/dev/null 2>&1; then\n" +
                 "        systemctl restart sshd\n" +
@@ -330,5 +342,9 @@ public class CommonUtils {
                 "runcmd:\n" +
                 "  - bash /tmp/setup_root_access.sh\n" +
                 "  - rm /tmp/setup_root_access.sh\n";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getPwdShell("123"));
     }
 }
